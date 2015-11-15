ad3ea3356ce5e639ff7e577f7243dfe8251181c8
DreamPiggy <lizhuoli1126@126.com>
2015-11-15 10:44:53 +0800

    merge

--- routes/push.js
+++ routes/push.js
@@ -2,7 +2,7 @@
  * Created by harvey on 2015/11/14.
  */
 
-//TODO receive msg pushed from rsp pi.
+//Deprecated receive msg pushed from rsp pi.
 function push(req,res,next){
     res.end('fine');
 }
