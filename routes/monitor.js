89b66f410c3af7a7236aa692c59878f1954275d6
DreamPiggy <lizhuoli1126@126.com>
2015-11-15 11:15:16 +0800

    merge
    merge

--- routes/monitor.js
+++ routes/monitor.js
@@ -2,22 +2,77 @@
  * Created by harvey on 2015/11/14.
  */
 var express = require('express');
+var request = require('superagent');
 var router = express.Router();
 var filter = require('../app/util/filter');
+var log = require('../app/util/log');
+var config = require('../config'),
+    raspURL = config.raspURL;
 
-//TODO get url of video stream
+/*
+ *  @deprecated
+ */
 router.get('/url',filter.authorize,function(req,res,next){
     res.end('fine');
 
 });
-//TODO start monitor
+//start monitor
 router.get('/start',filter.authorize,function(req,res,next){
-    res.end('fine');
-
+    var url = raspURL + "/monitor/image";
+    request
+        .get(url)
+        .end(function (err, resp) {
+            if (err) {
+                log.info('fuck');
+                log.info('start monitor failed,superagent error');
+                log.error(err.toString());
+                res.writeHead(403, {
+                    'content-type': 'text/plain'
+                });
+                res.end('start monitor failed,superagent error');
+            } else if (resp.statusCode == 200) {
+                res.writeHead(200, {
+                    'Content-type': 'text/plain'
+                });
+                log.info('start monitor successfully');
+                res.end('start monitor successfully');
+            } else {
+                res.writeHead(403, {
+                    'Content-type': 'text/plain'
+                });
+                log.info('start monitor failed');
+                res.end('start monitor failed');
+            }
+        });
 });
-//TODO stop monitor
+// stop monitor
 router.get('/stop',filter.authorize,function(req,res,next){
-    res.end('fine');
+    var url = raspURL + "/monitor/stop";
+    request
+        .get(url)
+        .end(function (err, resp) {
+            if (err) {
+                log.info('fuck');
+                log.info('stop monitor failed,superagent error');
+                log.error(err.toString());
+                res.writeHead(403, {
+                    'content-type': 'text/plain'
+                });
+                res.end('stop  monitor failed,superagent error');
+            } else if (resp.statusCode == 200) {
+                res.writeHead(200, {
+                    'Content-type': 'text/plain'
+                });
+                log.info('stop  monitor successfully');
+                res.end('stop  monitor successfully');
+            } else {
+                res.writeHead(403, {
+                    'Content-type': 'text/plain'
+                });
+                log.info('stop  monitor failed');
+                res.end('stop  monitor failed');
+            }
+        });
 
 });
 //TODO post schedule monitor
